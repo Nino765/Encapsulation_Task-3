03cbab960b2fb9eb4a59f9871071706082a828ca
Nino765 <angelninocarel@gmail.com>
2025-09-18 22:54:04 -0700

    feat(electronics: add electronics with unique property and method

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'product.dart';
+import 'electronics.dart';
 
 void main() {
   // Base class
   var prod = Product("P001", "Generic Item", 100);
   prod.showDetails();
   prod.applyDiscount(10);
+
+  // Derived: ElectronicProduct
+  var laptop = ElectronicProduct("E001", "Laptop", 50000, 24);
+  laptop.showDetails();
+  laptop.extendWarranty(6);
 }
